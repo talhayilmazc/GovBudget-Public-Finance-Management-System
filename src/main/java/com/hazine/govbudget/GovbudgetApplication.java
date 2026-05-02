e784ca71e18ac998232f96e271d75ff7350ea605
Talha Yılmaz <talhayilmazc@gmail.com>
2026-05-02 09:38:22 +0300

    chore: initial project setup with Spring Boot 3, Java 17

---
+++ src/main/java/com/hazine/govbudget/GovbudgetApplication.java
@@ -0,0 +1,13 @@
+package com.hazine.govbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovbudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovbudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hazine/govbudget/GovbudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazine.govbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovbudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
