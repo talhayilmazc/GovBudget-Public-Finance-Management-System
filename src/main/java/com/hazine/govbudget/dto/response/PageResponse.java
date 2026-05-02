598a09e94f52c430f864917638030bbec68dfd80
Talha Yılmaz <talhayilmazc@gmail.com>
2026-05-02 11:57:52 +0300

    feat: add generic PageResponse DTO for paginated API responses

---
+++ src/main/java/com/hazine/govbudget/dto/response/PageResponse.java
@@ -0,0 +1,19 @@
+package com.hazine.govbudget.dto.response;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PageResponse<T> {
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+}
